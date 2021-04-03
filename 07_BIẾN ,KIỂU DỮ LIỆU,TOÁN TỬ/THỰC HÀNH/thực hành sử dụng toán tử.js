7992e737840db1b7ad2b3ee60bccb3e19293960d
unknown <kinglionhhh@gmail.com>
2021-04-04 00:25:20 +0700

    commit bai 7 bien kieu du lieu toan tu

--- 07_BIẾN ,KIỂU DỮ LIỆU,TOÁN TỬ/THỰC HÀNH/thực hành sử dụng toán tử.js
+++
@@ -1,4 +0,0 @@
-let a=parseInt(prompt("inputWidth"));
-let b=parseInt(prompt("inputHeight"));
-let area =a*b
-alert("area is "+area)
---
+++ _07_bien_kieudulieu_toantu/thuc_hanh/thucHanhToanTu_js.js
@@ -0,0 +1,12 @@
+
+
+//thuc hanh toan tu
+let width= parseInt(prompt("inputWidth"));
+let height= parseInt(prompt("inputHeight"));
+let area = width*height;
+
+document.write("Enter width is: <br/>"+width );
+document.write("<br>")
+document.write("Enter height is: <br/>"+height);
+document.write("<br>")
+document.write("The area is: <br/>"+area);
